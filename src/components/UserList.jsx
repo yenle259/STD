55a6c57f29c486d564d9255f0ff9e477012d145d
Yen Le <vile0229@gmail.com>
2021-08-14 15:57:05 +0700

    render user

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
+import useUsers from '../hooks/useUsers'
 import UserItem from './UserItem'
 
 function UserList() {
+     const [users,loading,success,failed]= useUsers();
+
+    const renderUsers = ()=> {
+        return users.map(({name,id,avatar}) =>(
+            <UserItem  key={id} name={name} avatar={avatar}/>
+        ))
+    }
     return (
         <div>
             <h2>User List</h2>
-            <UserItem name="username" avatar="https://picsum.photos/200"/>
-            <UserItem/>
-            <UserItem/>
+            {loading && <em>Loading...</em>}
+            {failed && <em>Fetch data failed</em>}
+            {success && !users && <em>No data here</em>}
+            {success && users && renderUsers()}
+           
         </div>
     )
 }
---
+++ src/hooks/useUsers.js
@@ -0,0 +1,24 @@
+
+import {useEffect,useState} from "react";
+function useUsers() {
+    const [loading, setLoading] = useState(false);
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false);
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        setLoading(true);
+        fetch('https://61176b1c30022f0017a05dfa.mockapi.io/api/v1/users')
+            .then(res=>res.json())
+            .then(json=>{
+                setData(json);
+                setSuccess(true);
+            })
+            .catch(error=>setError(true))
+            .finally(()=>setLoading(false))
+    }, [])
+
+    return [data,loading,success,error];
+}
+
+export default useUsers
