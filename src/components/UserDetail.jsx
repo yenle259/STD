1bafb184955aad6e8505f0029a48d0f99a779fa3
Yen Le <vile0229@gmail.com>
2021-08-17 20:16:08 +0700

    useEffect v useState

--- src/components/UserDetail.jsx
+++ src/components/UserDetail.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
+import useUserById from '../hooks/useUserById';
 
 function UserDetail() {
     const {id} =useParams();
+    const [data,loading,success,error] = useUserById(id);
+    const hasData = data!= null;
+
+    const renderUser =()=>{
+        const {avatar,createdAt, name} = data;
+        return <div>
+            <img src={avatar} alt =""/>
+            <h3>{name}</h3>
+            <p>{createdAt}</p>
+        </div>
+    };
     return (
         <div>
-            UserDetail: {id}
+            <h3>User Detail</h3>
+            <hr/>
+            {loading && <em>Loading...</em>}
+            {error && <em>Fetch data failed</em>}
+            {success && !hasData && <em>No data here</em>}
+            {success && hasData && renderUser()}
+
         </div>
     )
 }
